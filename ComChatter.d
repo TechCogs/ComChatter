975262e210f0704160013e3fe63f60aba9892fa8
Tech Cogs <48573028+TechCogs@users.noreply.github.com>
2019-03-15 20:23:34 -0700

    Added reply to ping request

--- ComChatter.d
+++ ComChatter.d
@@ -1,4 +1,14 @@
+/***********************************************************************
+ * ComChatter, A very simple command line IRC client                   *
+ * Copyright (C) 2019 TechCogs                                         *
+ * Licensed under the GNU GPLv3                                        *
+ *                                                                     *
+ * @TechCogs on Twitter and Github                                     *
+ * https://github.com/TechCogs/ComChatter                              *
+ ***********************************************************************/
+
 import std.stdio;
+import std.string;
 import std.socket;
 
 void main() {
@@ -7,8 +17,8 @@ void main() {
     auto address = getAddress("irc.freenode.org", 6667);
     sock.connect(address[0]);
 
-    sock.send("NICK Thingy\r\n");
-    sock.send("USER Thingy * * :Thingy\r\n");
+    sock.send("NICK ReallyNThingy\r\n");
+    sock.send("USER ReallyNThingy * * :Thingy\r\n");
 
     while(true) {
       char[] input;
@@ -17,6 +27,10 @@ void main() {
       while(buf[0] != '\n') {
         sock.receive(buf);
         input ~= buf;
+      }
+
+      if (input.startsWith("PING")) {
+        sock.send("PONG\r\n");
       }
 
       write(input);
